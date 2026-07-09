98a936068a44441cb8c38d89668eff69727051c6
Jobs <alexander.v.eremeev@gmail.com>
2026-07-09 13:38:51 +0300

    Update Libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.childmathematics.android.workschedule"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
